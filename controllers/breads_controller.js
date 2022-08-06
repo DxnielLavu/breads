3c367b35a1417351f7b78447dbe32912b47e8b8b
Daniel <dbaray2118@gmail.com>
2022-08-06 11:11:43 -0700

    titles

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -3,12 +3,14 @@ const breads = express.Router();
 const Bread = require("../models/bread.js");
 
 // INDEX
-breads.get("/", (req, res) => {
-  res.render("Index", {
-    breads: Bread,
-  });
-  // res.send(Bread)
-});
+breads.get('/', (req, res) => {
+    res.render('Index',
+      {
+        breads: Bread,
+        title: 'Index Page'
+      }
+    )
+})
 
 module.exports = breads;
 
