fb731e271fdfb3161f12e571d838453878f7293f
Daniel <dbaray2118@gmail.com>
2022-08-20 10:43:31 -0700

    BreadCRUD Second Model

--- models/bread.js
+++ models/bread.js
@@ -5,14 +5,15 @@ const { Schema } = mongoose
 
 // schema
 const breadSchema = new Schema({
-    name: { type: String, required: true },
-    hasGluten: Boolean,
-    image: { type: String, default: 'https://placekitten.com/500/500' },
-    baker: {
-      type: String,
-      enum: ['Rachel', 'Monica', 'Joey', 'Chandler', 'Ross', 'Phoebe']
-    }
+  name: { type: String, required: true },
+  hasGluten: Boolean,
+  image: { type: String, default: 'https://placekitten.com/500/500' },
+  baker: {
+    type: Schema.Types.ObjectID,
+    ref: 'Baker'
+  }
 })
+
 
 // helper methods 
 breadSchema.methods.getBakedBy = function(){
